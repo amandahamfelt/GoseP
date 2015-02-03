61ea9619e5132e2bfccb44e0bd552439125a3b25
amandasjostrom <amandasjostrom@hotmail.com>
2015-02-03 12:33:21 +0100

    Testar ett filter

--- main.js
+++ main.js
@@ -1,9 +1,24 @@
 window.onload = function(){
+ 
 document.getElementById('filter').addEventListener("click", applyFilter);
+$( ".filter" ).click(function() {
+alert( "Handler for .click() called on:" + this.id);
+});
 }
 
 function applyFilter(){
-    alert();
+    //alert();
+    var canvas=document.getElementById("drawing"); // grabs the canvas element
+    var context=canvas.getContext("2d"); // returns the 2d context object
+
+    var imgData = context.getImageData(0,0,canvas.width, canvas.height);
+    for (var i = 0; i < imgData.data.length; i+=4) {
+        imgData.data[i]=255-imgData.data[i];
+        imgData.data[i+1]=255-imgData.data[i+1];
+        imgData.data[i+2]=255-imgData.data[i+2];
+        /*imgData.data[i+3]=255;*/
+    };
+    context.putImageData(imgData,0,0);
 }
 
 function readURL(input) { console.log('readURL');
@@ -23,6 +38,7 @@ function readURL(input) { console.log('readURL');
                 context.drawImage(img,0,0); 
                 console.log('width' + img.width);}
                 
+
             };
 console.log('klar');    
             reader.readAsDataURL(input.files[0]);      
@@ -30,6 +46,8 @@ console.log('klar');
 }
 
 Filters = {};
+
+
 Filters.getPixels = function(img) {
   var c = this.getCanvas(img.width, img.height);
   var ctx = c.getContext('2d');
